7b5b7c0abb3047ce858525c31167606d7a0dc4b3
mohamedabdalhamied139 <mohamedabdalhamied139@gmail.com>
2026-09-18 17:12:52 +0300

    refactor(mobile): wipe previous mobile implementation to restart 100% faithful architecture from scratch

--- mobile/lib/games/adapters/game_adapter.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class GameWidgetAdapter extends Widget {
-  const GameWidgetAdapter({super.key});
-
-  void handleEvent(Map<String, dynamic> event);
-}
