571fc7d81901b756cf3418361c49f08d9a1477ff
mohamedabdalhamied139 <mohamedabdalhamied139@gmail.com>
2026-09-19 07:34:15 +0300

    refactor(mobile): remove Activity Log icon button in favor of two-finger swipe gesture

--- mobile/lib/views/responsive_shell.dart
+++ mobile/lib/views/responsive_shell.dart
@@ -4,14 +4,11 @@ import '../core/localization.dart';
 import '../widgets/two_finger_gesture_detector.dart';
 import 'activity_log_widget.dart';
 
-/// Responsive shell wrapping mobile views with the two-finger swipe right gesture
-/// and top AppBar with an Activity Log toggle action.
 class ResponsiveShell extends StatelessWidget {
   final String title;
   final Widget child;
   final List<Widget>? actions;
   final Widget? floatingActionButton;
-  final bool showActivityLogAction;
   final Widget? leading;
   final PreferredSizeWidget? bottom;
 
@@ -21,34 +18,18 @@ class ResponsiveShell extends StatelessWidget {
     required this.child,
     this.actions,
     this.floatingActionButton,
-    this.showActivityLogAction = true,
     this.leading,
     this.bottom,
   });
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> effectiveActions = [];
-    if (actions != null) {
-      effectiveActions.addAll(actions!);
-    }
-
-    if (showActivityLogAction) {
-      effectiveActions.add(
-        IconButton(
-          icon: const Icon(Icons.chat_bubble_outline, color: AppColors.textPrimary),
-          tooltip: tr('سجل الأحداث والدردشة'),
-          onPressed: () => ActivityLogWidget.showAsBottomSheet(context),
-        ),
-      );
-    }
-
     return Scaffold(
       backgroundColor: AppColors.background,
       appBar: AppBar(
         title: Text(title),
         leading: leading,
-        actions: effectiveActions,
+        actions: actions,
         bottom: bottom,
       ),
       // Two-finger swipe right anywhere opens the Activity Log per explicit user instruction
